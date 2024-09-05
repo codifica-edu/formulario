23ab26b84f6f4f8450310e851073ace5f91ddf2e
Codifica <contato@codificaedu.com.br>
2024-09-05 16:18:40 -0300

    adição de comentários

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function enviar() {
+    // Pegando os valores dos inputs
     let nome = document.forms["infos"]["nome"].value;
     let email = document.forms["infos"]["email"].value;
     let idade = document.forms["infos"]["idade"].value;
     let telefone = document.forms["infos"]["telefone"].value;
     
+    // Exibindo os valores dos inputs
     document.getElementById('resultadoNome').textContent = `Nome: ${nome}`;
     document.getElementById('resultadoEmail').textContent = `E-mail: ${email}`;
     document.getElementById('resultadoIdade').textContent = `Idade: ${idade}`;
